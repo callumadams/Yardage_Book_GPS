84627dd1dc0338d19b70355ea5175e9af9e8c403
callumadams <callum.adams11@gmail.com>
2018-05-15 12:53:30 +1000

    Initial commit

---
+++ app/src/main/java/greengrid/example/android/yardagebookgps/GPSLogScreen.java
@@ -0,0 +1,17 @@
+package greengrid.example.android.yardagebookgps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GPSLogScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gpslog_screen);
+    }
+
+    private void test(){
+        int i = 1+1;
+    }
+}
